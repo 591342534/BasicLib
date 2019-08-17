2841c7ed3eeddc68139fcafbf9c43e36755323cf
daniel <591342534@qq.com>
2019-08-17 16:25:50 +0800

    openssl cert and src project

---
+++ openssl_test/code/openssl_sever/main.cpp
@@ -0,0 +1,21 @@
+//#include <QCoreApplication>
+
+//int main(int argc, char *argv[])
+//{
+//    QCoreApplication a(argc, argv);
+
+//    return a.exec();
+//}
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+
+int main(int argc, char *argv[])
+{
+    printf("welcome, this is openssl_server\n");
+
+
+    return 0;
+}
